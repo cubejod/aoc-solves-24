cb793aad9204ceff85de1efe9481a849c631eb41
cubejod <notblankname@gmail.com>
2024-12-03 00:48:05 -0500

    added template

---
+++ template.py
@@ -0,0 +1,16 @@
+import copy
+import heapq
+import math
+import collections
+d = open("day1input.txt").read().strip()
+l = d.split("\n")
+op = 0
+op2 = 0
+for line in l:
+    # nums = list(map(int, line.split()))
+    # a, b = line.split()
+    pass
+
+
+print(op)
+print(op2)
