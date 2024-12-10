87ba6005835c1eafee413717907663d241943297
cubejod <notblankname@gmail.com>
2024-12-10 00:30:34 -0500

    day 10 fixed

--- day10.py
+++ day10.py
@@ -1,3 +1,4 @@
+# sadly started late today, i was quite fast with this. code is actually decent too
 import copy
 import heapq
 import math
@@ -12,21 +13,22 @@ for i in range(len(l)):
     for j in range(len(l[i])):
         if l[i][j] == "0":
             toCheck.append((i, j, 0))
-while toCheck:
-    cur = toCheck.popleft()
-    if cur[2] == 9:
-        op += 1
-    else:
-        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        for i in range(len(dirs)):
-            ni = cur[0]+dirs[i][0]
-            nj = cur[1]+dirs[i][1]
-
-            if ni >= 0 and ni < len(l) and nj >= 0 and nj < len(l[0]):
-                if l[ni][nj] != ".":
-                    if int(l[ni][nj]) == cur[2]+1:
-                        toCheck.append((ni, nj, cur[2]+1))
-
-
+            seen = set()
+            while toCheck:
+                cur = toCheck.popleft()
+                if cur[2] == 9:
+                    if (cur[0], cur[1]) not in seen:
+                        op += 1
+                        seen.add((cur[0], cur[1]))
+                    op2 += 1
+                else:
+                    dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+                    for k in range(len(dirs)):
+                        ni = cur[0]+dirs[k][0]
+                        nj = cur[1]+dirs[k][1]
+                        if ni >= 0 and ni < len(l) and nj >= 0 and nj < len(l[0]):
+                            if l[ni][nj] != ".":
+                                if int(l[ni][nj]) == cur[2]+1:
+                                    toCheck.append((ni, nj, cur[2]+1))
 print(op)
 print(op2)
